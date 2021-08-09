fe4606b0572e1e80447a5ffd34076b57b2a0e9b6
DevDoge <lhuthefranc@hotmail.com>
2021-08-09 17:03:12 -0300

    Criação da classe LogUtil.java e atição de log4j.properties

--- src/main/java/br/com/jogoshowmilhao/application/Main.java
+++ src/main/java/br/com/jogoshowmilhao/application/Main.java
@@ -1,20 +1,26 @@
 package br.com.jogoshowmilhao.application;
 
+import br.com.jogoshowmilhao.util.LogUtil;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Main extends Application {
+
+    private static final String VIEW_FXML = "views/hello-view.fxml";
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("views/hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(this.getClass().getResource(VIEW_FXML)));
+        primaryStage.setTitle("Hello World");
+        LogUtil.getLogger(Main.class).info(primaryStage.getTitle());
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
--- src/main/java/br/com/jogoshowmilhao/util/LogUtil.java
+++ src/main/java/br/com/jogoshowmilhao/util/LogUtil.java
@@ -1,14 +1,16 @@
 package br.com.jogoshowmilhao.util;
+
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
 public class LogUtil {
-    public static final String LOG4J_PROPERTIES = "br/com/jogoshowmilhao/application/log4j.properties";
+    public static final String LOG4J_PROPERTIES = "log4j.properties";
 
     public LogUtil() {
     }
     public static Logger getLogger(Object object){
-        PropertyConfigurator.configure(LogUtil.class.getClassLoader().getResource(LOG4J_PROPERTIES));
+        PropertyConfigurator.configure(LogUtil.class.
+                getClassLoader().getResource(LOG4J_PROPERTIES));
     return Logger.getLogger(object.getClass());
     }
 }
